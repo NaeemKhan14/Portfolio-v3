f01d8b5837ae3805b099625181041e4e3dc40f88
Naeem Khan <naeemukhan14@gmail.com>
2025-07-02 23:03:59 +0200

    change link color to match the theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,15 +51,16 @@ export default function RootLayout({
             </main>
             <ScrollToTop />
             <footer className="w-full flex items-center justify-center py-3">
-              <span className="pr-1">© 2022 All Rights Reserved. </span>
+              <span className="pr-1">© 2022 All Rights Reserved.</span>
+              <span className="text-default-600 pr-1">Powered by</span>
+              
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
                 href="https://heroui.com?utm_source=next-app-template"
                 title="heroui.com homepage"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <p className="text-danger hover:underline">HeroUI</p>
               </Link>
             </footer>
 
