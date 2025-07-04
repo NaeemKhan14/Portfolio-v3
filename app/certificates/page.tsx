21df3ef09809c86fc839a90fe7cce3393162ed73
Naeem Khan <naeemukhan14@gmail.com>
2025-07-05 01:09:39 +0200

    Changed styling of certificates page

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -5,19 +5,18 @@ import { Divider } from "@heroui/react";
 
 export default function CertificatesPage() {
   return (
-    <div className="py-6">
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-bold text-center text-gray-900 dark:text-white mb-10">
-          All Certificates
+      // <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col md:max-w-2xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Certificates
         </h1>
-        <Divider className="mb-6" />
+        <Divider className="mb-8" />
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 gap-8">
           {certificates.map((cert, idx) => (
             <CertificateCardMain key={idx} {...cert} />
           ))}
         </div>
       </div>
-    </div>
   );
 }
