f105c87799a3f196089bcb266b137a2fba65cd8d
Naeem Khan <naeemukhan14@gmail.com>
2025-07-23 12:46:07 +0200

    Styled the global error page

--- Application/Frontend/app/error.tsx
+++ Application/Frontend/app/error.tsx
@@ -10,22 +10,29 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
-    /* eslint-disable no-console */
     console.error(error)
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
+    <section className="mx-auto flex flex-col md:max-w-2xl text-center">
+        <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
+          Something went wrong
+        </h1>
+
+        {error?.message && (
+          <p className="mt-4 text-sm text-gray-600 dark:text-gray-400">
+            {error.message}
+          </p>
+        )}
+
+        <div className="mt-3">
+          <button
+            onClick={reset}
+            className='bg-danger rounded-lg px-4 py-2 text-center text-sm text-white transition-colors hover:bg-pink-300 dark:hover:bg-pink-900'
+          >
+            Try again
+          </button>
+        </div>
+    </section>
   )
 }
