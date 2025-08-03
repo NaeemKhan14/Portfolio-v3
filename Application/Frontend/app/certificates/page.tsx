2cad3795d23197e794e3a6fb9a44035e2e41342e
Naeem <naeemukhan14@gmail.com>
2025-08-03 15:24:20 +0200

    Moved the return statement inside try{} block to fix rendering logic

--- Application/Frontend/app/certificates/page.tsx
+++ Application/Frontend/app/certificates/page.tsx
@@ -3,30 +3,29 @@ import { fetchFromApi } from '@/lib/api-fetcher'
 import { Divider } from '@heroui/react'
 
 export default async function CertificatesPage() {
-  const certificates: Certificate[] = []
 
   try {
     const data = await fetchFromApi<Certificate>('/certificates?sort=-date')
     const certificates = data.docs
+
+    return (
+      <div className='mx-auto flex flex-col md:max-w-2xl'>
+        <h1 className='mb-6 text-center text-3xl font-bold'>Certificates</h1>
+        <Divider className='mb-8' />
+        {certificates.length === 0 ? (
+          <p className='text-center text-gray-600 dark:text-gray-300'>
+            No Certificates.
+          </p>
+        ) : (
+          <div className='grid grid-cols-1 gap-8'>
+            {certificates.map((cert, idx) => (
+              <CertificateCard key={idx} {...cert} />
+            ))}
+          </div>
+        )}
+      </div>
+    )
   } catch (error) {
     throw new Error('Error in retrieving certificates from the server')
   }
-
-  return (
-    <div className='mx-auto flex flex-col md:max-w-2xl'>
-      <h1 className='mb-6 text-center text-3xl font-bold'>Certificates</h1>
-      <Divider className='mb-8' />
-      {certificates.length === 0 ? (
-        <p className='text-center text-gray-600 dark:text-gray-300'>
-          No Certificates.
-        </p>
-      ) : (
-        <div className='grid grid-cols-1 gap-8'>
-          {certificates.map((cert, idx) => (
-            <CertificateCard key={idx} {...cert} />
-          ))}
-        </div>
-      )}
-    </div>
-  )
 }
