197fece59f696617fe7aba694eaae2ba9fe909b7
Naeem <naeemukhan14@gmail.com>
2025-08-04 15:25:04 +0200

    Changed loading state wrapper's logic to work with all new pages

--- Application/Frontend/components/ui/EmptyPageWrapper.tsx
+++ Application/Frontend/components/ui/EmptyPageWrapper.tsx
@@ -3,18 +3,26 @@ import { Suspense, ReactNode } from 'react'
 import LoadingSpinner from '@/components/ui/LoadingSpinner'
 
 interface EmptyPageLayoutProps {
-    title: string
+    title?: string
     children: ReactNode
 }
 
 export default function EmptyPageLayout({ title, children }: EmptyPageLayoutProps) {
     return (
-        <div className={`mx-auto flex flex-col md:max-w-2xl`}>
-            <h1 className='mb-6 text-center text-3xl font-bold'>{title}</h1>
-            <Divider className='mb-8' />
-            <Suspense fallback={<LoadingSpinner />}>
-                {children}
-            </Suspense>
-        </div>
+        <>
+            {title
+                ?
+                <div className={`mx-auto flex flex-col md:max-w-2xl`}>
+                    <h1 className='mb-6 text-center text-3xl font-bold'>{title}</h1>
+                    <Divider className='mb-8' />
+                    <Suspense fallback={<LoadingSpinner />}>
+                        {children}
+                    </Suspense>
+                </div>
+                :
+                <Suspense fallback={<LoadingSpinner />}>
+                    {children}
+                </Suspense>}
+        </>
     )
 }
